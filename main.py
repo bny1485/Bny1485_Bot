c666c56fbf69920fcbacca7edcccfe898501fb48
bny1485 <benyaminmahmoudyan@gmail.com>
2020-08-16 16:22:22 +0430

    not call

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('Hello world and git and github.')
+# This is main file
+
+# telebot telegram api 
+# install it with pip3 install pyTelegramBotAPI 
+# you can call in in python file with import telebot
+
+import telebot
+
+class main_bot():
+    
+    Auther = 'Benyamin'
+    Email = 'benyaminmahmoudyan@gmail.com'
+    phone = '+98-910-966-7550'
+    tele_user_name = '@bny1485'
+
+    def __init__(self, token, your_msg):
+        self.token = token
+        self.your_msg = your_msg
+
+    def about_me(self, Auther, Email, phone):
+        msg = f'I am {Auther} my email address is {Email} and this is my phone number {phone}'
+        return msg
+
+    def it_is_work(self, your_mas, tele_user_name):
+        return f'I am Bny1485 Bot you can contact my owner with his telegram user name {tele_user_name} your token is {token} and your massge to me is {your_msg}'
+
+
+token = input('Welcome to Bny1485_Bot\nplase give me your bot token: ')
+user_msg = input('what is your massge for Auther : ')
+
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # this is my bot readme
 
-## in this file i tech you how to use my bot
+## in this file i tech you how to use my bot and some other text
+
+this is my first github upload and i want to know how dose it work and see git, github tools for project manager and be familar with them 
+
+If you are reading this text, thank you for emailing your tips to me.
+Thanks a lot
