c5d3751203250d7c33d228c4e0adb9a247c5a3be
bny1485 <benyaminmahmoudyan@gmail.com>
2020-08-26 12:37:56 +0430

    change bot

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 ### --------------------- This is main file -------------------------- ###
-
-# telebot telegram api 
-# install it with pip3 install pyTelegramBotAPI 
+# telebot telegram api
+# install it with pip3 install pyTelegramBotAPI
 # you can call in in python file with import telebot
 
 import telebot
 
+bot_token = "1211332991:AAFEJylXhEOfLanJavEkJVorQ0umHNmoK2w"
+bot = telebot.TeleBot(bot_token)
 
-## -------- in this two line get token form "@botfather" and define bot  ------- ##
-token =  "1354592892:AAHdnEWxWPCPe39LCB1yRjxi1fojVzpQQ3s"
-bot = telebot.TeleBot(token)
-print('I AM start ...')
+msg = ['I am Benyamin my email address is benyaminmahmoudyan@gmail.com \
+    and this is my phone number +98-910-966-7550 you can contact me in in t \
+    elegram by @bny1485 instagram with this id @bny1485', 'my name is seventh robot I am from Seventh Regiment']
+
 
 ## ------ this function tell abot programmmer of this bot -------##
-@bot.message_handler(commands=['Auther'])
-def Hi(user):    
+@bot.message_handler(command=['Auther'])
+def Hi(user):
     usr_id = user.from_user.id
     bot.send_message(usr_id, "Yes, dear 😉🌹")
+    global msg
+    bot.send_message(usr_id, msg[0])
 
-    msg = 'I am Benyamin my email address is benyaminmahmoudyan@gmail.com \
-    and this is my phone number +98-910-966-7550 you can contact me in in t \
-    elegram by @bny1485 instagram with this id @bny1485'
-    
-    bot.send_message(usr_id, msg)
 
 ## ------- help and command massege ------- ##
-@bot.message_handler(commands=['start','help'])
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, 'Hi. welcome to my bot 👋')
+    global msg
+    if message == "start":
+        bot.reply_to(message, 'Hi. welcome to my bot 👋')
+    elif message == "help":
+        bot.reply_to(message, msg[1])
 
 
-
-# this bilt in function to keep bot runing
 bot.polling()
